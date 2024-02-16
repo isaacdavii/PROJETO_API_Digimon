1584821053a3ea4671fe051c7504b82965c96ca7
Isaac Davi <izaacddavid98@gmail.com>
2024-02-15 23:43:03 -0300

    Projeto API Digimon

---
+++ app.js
@@ -0,0 +1,38 @@
+function populateDigimonNames() {
+    var level = document.getElementById("level").value;
+    fetch(`https://digimon-api.vercel.app/api/digimon/level/${level}`)
+        .then(response => response.json())
+        .then(data => {
+            var nameSelect = document.getElementById("name");
+            nameSelect.innerHTML = "";
+            data.forEach(digimon => {
+                var option = document.createElement("option");
+                option.text = digimon.name;
+                option.value = digimon.name;
+                nameSelect.add(option);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function getDigimon() {
+    var level = document.getElementById("level").value;
+    var name = document.getElementById("name").value;
+    fetch(`https://digimon-api.vercel.app/api/digimon/name/${name}`)
+        .then(response => response.json())
+        .then(data => {
+            mostrarDigimon(data[0]);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function mostrarDigimon(digimon) {
+    var digimonInfo = document.getElementById("digimonInfo");
+    digimonInfo.innerHTML = `
+    <h2>${digimon.name}</h2>
+    <p>Level: ${digimon.level}</p>
+    <img src="${digimon.img}" alt="${digimon.name}">
+`;
+}
+
+populateDigimonNames();
